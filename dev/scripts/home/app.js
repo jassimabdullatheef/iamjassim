a22ccd2fc7b812e19f7f8d152a8182b6813257a4
Jassim Abdul Latheef <jassimabdullatheef@gmail.com>
2015-09-28 16:39:00 +0300

    Added Bootstrap, angular routing

--- dev/scripts/home/app.js
+++ dev/scripts/home/app.js
@@ -1,4 +1,30 @@
-var app = angular.module('iamjassim',[]);
+var app = angular.module('iamjassim',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+  $routeProvider
+    .when('/',{
+      templateUrl : 'partials/home.html',
+      controller : 'mainController'
+    })
+    .when('/works',{
+      templateUrl : 'partials/works.html',
+      controller : 'mainController'
+    })
+    .when('/me',{
+      templateUrl : 'partials/me.html',
+      controller : 'mainController'
+    })
+    .when('/connect',{
+      templateUrl : 'partials/connect.html',
+      controller : 'mainController'
+    })
+    .otherwise({
+      templateUrl : 'partials/home.html',
+      controller : 'mainController'
+    });
+
+}]);
+
 app.controller('mainController',['$scope',function($scope){
   $scope.name = 'Jassim';
 }]);
